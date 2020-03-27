fbeabdc706aed30a81beef33f456dabb17fe3da9
fxw13 <49396443+fxw13@users.noreply.github.com>
2020-03-27 20:45:09 +0800

    Add files via upload

---
+++ cos_se.m
@@ -0,0 +1,20 @@
+function cos_se = cos_se( A )
+%% cos（x)= 1 - x^2/2! + x^4/4! 使用泰勒级数计算cos函数
+x = (A/180)*pi;%把输入的角度转换为弧度
+ 
+%%
+symbol=1;
+sum=0;
+my_eps=10^-8;
+temp_sum=1;
+power=0;
+while abs(temp_sum)>my_eps
+    sum=sum+temp_sum;
+    symbol=-symbol;
+    power=power+2;
+    temp_sum=symbol*x^power/factorial(power);
+    
+end
+cos_se = num2str(sum,4);
+end
+
