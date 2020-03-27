671a0b949094a6df22a70280642be5ea8b3dbe9d
HQianCQU <61692462+HQianCQU@users.noreply.github.com>
2020-03-27 20:36:36 +0800

    Add files via upload

---
+++ cot_se.m
@@ -0,0 +1,23 @@
+function [t]=cot_se(s) %采用cordic算法实现
+%初始化
+x = 1;
+y = 0;
+z = s * pi /180;%待求角度θ
+a = 0;
+d = 1;
+k = 0.6073; %k为增益
+x = k*x;
+while a<100 %此处不能判断d的负号控制循环，会死循环。应用a次数控制。
+    if z>=0
+        d = 1;
+    else d = -1;
+    end
+%迭代
+    xNew = x;
+    x = xNew-(y*d*(1/2^a));
+    y = y+(xNew*d*(1/2^a));
+    z = z-(d*(atan(1/2^a)));
+    a = a+1;
+end
+t = x/y;
+end
