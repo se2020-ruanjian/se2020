e90ee28fef869f12d3ae2a8fd022e84433b35ecf
fxw13 <49396443+fxw13@users.noreply.github.com>
2020-04-06 13:45:52 +0800

    Add files via upload

--- cos_se.c
+++ cos_se.c
@@ -2,34 +2,28 @@
 #include <math.h>
 #include <stdlib.h>
 
-#define PI 3.141592
-double Factorial(int n)
-{
-	//long long Õ¼8¸ö×Ö½Ú
-	double i, factorial = 1;
+float cos_se(float);
 
-	for (i = 1; i <= n ; i++)
-	{
-		factorial *= i;
-	}
-	
-	return factorial;
-}
-
-double cos_se(double x)
+float cos_se(float x)
 {
-	double cosx = 1.0;
+    #define PI 3.141592
+	float cosx = 1.0;
 	int m;
 	x = (fmod(x,360) * PI / 180);
 	for(m = 1; m <= 12; m++)
 	{
+		float i, factorial = 1;
+		for (i = 1; i <= 2*m ; i++)
+	{
+		factorial *= i;
+	}
 		if (m % 2 == 0)
 		{
-			cosx += pow(x, (2 * m )) / Factorial(2 * m );
+			cosx += pow(x, (2 * m )) / factorial;
 		}
 		else
 		{
-			cosx -= pow(x, (2 * m )) / Factorial(2 * m );
+			cosx -= pow(x, (2 * m )) /factorial;
 		}
 	}
 	return cosx;
